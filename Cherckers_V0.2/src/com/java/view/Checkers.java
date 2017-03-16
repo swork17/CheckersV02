9910055f5dc72b75f88552bb3bc4ce95e8ba725c
Lanxss <fabien226@hotmail.fr>
2017-03-16 16:00:08 +0100

    Ajout du menu et du score

--- Cherckers_V0.2/src/com/java/view/Checkers.java
+++ Cherckers_V0.2/src/com/java/view/Checkers.java
@@ -2,7 +2,6 @@ package com.java.view;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
-import java.awt.EventQueue;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.InputEvent;
@@ -41,12 +40,79 @@ public class Checkers extends JFrame
     				  board.add(new Checker(CheckerType.RED_REGULAR), i, j);
     	  }
       }
-      setContentPane(board);
+      
+      init_menu();
+      add(board);
+      init_score();
+    	  
       pack();
       setVisible(true);
 	  setLocationRelativeTo(null);
 
    }
    
+	private void init_menu() {
+		JMenuBar bar = new JMenuBar();
+		
+		JMenu file = new JMenu("Fichier");
+		JMenuItem new_game = new JMenuItem("Nouvelle partie" );
+		JMenuItem quit = new JMenuItem("Quitter" );
+			
+		JMenu game = new JMenu("Jeu");
+		JMenuItem createServer = new JMenuItem("Organiser");
+		JMenuItem join = new JMenuItem("Rejoindre");
+		JMenuItem save = new JMenuItem("Sauvegarder");
+		JMenuItem load = new JMenuItem("Charger");
+		JMenuItem restart = new JMenuItem("Recommencer");
+		
+		JMenu about = new JMenu("A propos");
+		JMenuItem rules = new JMenuItem("Regle du jeu" );
+		JMenuItem help = new JMenuItem("?" );
+		
+		file.add(new_game);
+		file.addSeparator();
+		file.add(quit);
+		game.add(createServer);
+		game.add(join);
+		game.add(save);
+		game.add(load);
+		game.add(restart);
+		about.add(rules);
+		about.add(help);
+		
+		bar.add(file);
+		bar.add(game);
+		bar.add(about);
+		
+		// Option "Nouvelle partie"
+		new_game.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_N, InputEvent.CTRL_MASK));
+		
+		// Option "Quitter"
+		quit.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_W, KeyEvent.CTRL_MASK));
+		
+		quit.addActionListener(new ActionListener(){
+	    	public void actionPerformed(ActionEvent e){
+	    		System.exit(0);
+	    	}
+	    });
+		
+		// Option "?"
+		help.addActionListener(new ActionListener(){
+	    	public void actionPerformed(ActionEvent e){
+    		JOptionPane.showMessageDialog(null, "Createurs : Kevin BOUCHER - Fabien DIDIER\nVersion : 0.1",
+						    		            "Information", JOptionPane.NO_OPTION);;
+	    	}
+	    });
+
+		add(bar,BorderLayout.NORTH);
+	}
+	
+	private void init_score() {
+
+		JPanel score = new JPanel();
+		score.add(new JLabel("Score : 0"));
+		score.setBorder(BorderFactory.createLineBorder(Color.black));
+		add(score, BorderLayout.SOUTH);
+	}  
 
 }
